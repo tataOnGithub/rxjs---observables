4352deb9060af790e56a7da35422334abff3b871
tatas-creativity <tatas.creativity@gmail.com>
2022-04-04 05:03:45 +0400

    first commit

---
+++ index.ts
@@ -0,0 +1,39 @@
+import { observable, of, from, interval, concat, filter, map, Observable } from "rxjs";
+
+//1
+const a$ = of(1, "aa", 3);
+const b$ = of(4, 5, "bb");
+const c$ = concat(a$, b$);
+c$.pipe(map((x: string | number) => {
+    if (typeof x === 'number') {
+        return x * 10;
+    }
+    return x + x;
+})).subscribe((x: string | number) => console.log(x))
+
+//2
+const obs$ = interval(1000);
+obs$.pipe(filter((x: number) => x % 2 === 0), map((x: number) => x * 2)).subscribe((x: number) => console.log(x));
+
+//3
+interface User {
+    firstName: string;
+    lastName: string;
+    age: number;
+}
+
+const users: User[] = [
+    {
+        firstName: 'giorgi',
+        lastName: 'bazerashvili',
+        age: 25
+    },
+    {
+        firstName: 'meore',
+        lastName: 'giorgi',
+        age: 17
+    }
+];
+
+const users$ = from(users);
+users$.pipe(filter((x: User) => x.age >= 18)).subscribe((x: User) => console.log(x));
